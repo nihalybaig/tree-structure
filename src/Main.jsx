fe455a00c40922402e796f2651ee13c5eb1ccf99
nihalybaig <nihal.y.baig@gmail.com>
2020-01-30 03:39:13 +0530

    NnN: toggle nodes

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import { CeoData as data } from './constants/ProcessData';
 
+const toggleTree = (e) => {
+  let el = document.getElementById(e.target.id).parentElement.childNodes[1];
+  let showEl = el.style.display;
+  (showEl==="none" || showEl==="")?
+    el.style.display="inline-block"
+    :
+    el.style.display="none"
+}
+
 const createTree = (data, lev) => {
   let level = lev || 0;
   let children = [];
@@ -8,8 +17,8 @@ const createTree = (data, lev) => {
       if (d.childs && d.childs.length) { // has child?
           level++;
           children.push(
-              <div key={d._id} className={"level-" + (level)}>
-                  <div className="node">{d.name}</div>
+              <div key={d._id} className={"level level-" + (level)}>
+                  <div className="node" id={d._id} onClick={(e)=>toggleTree(e)}>{d.name}</div>
                   {createTree(d.childs, level)}
               </div>
           );
@@ -20,11 +29,16 @@ const createTree = (data, lev) => {
               </div>
           );
       }
+      return null;
   })
   return <div className='tree-of-nodes'>{children}</div>;
 }
 
 export class Main extends Component {
+  componentDidMount(){
+    // quick hack
+    document.getElementsByClassName('App-header')[0].children[0].childNodes[0].style.display = 'inline-block';
+  }
   render() {
     return (
       <div>
